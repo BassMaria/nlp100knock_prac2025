74003a0bf5f3e6b3a2c4be147683e86df5c87b9c
shimizunobuki <bassmaria1117@gmail.com>
2025-10-31 21:23:54 +0900

    6終わり

---
+++ ch01/6.py
@@ -0,0 +1,27 @@
+def n_gram_character(target, n):
+    output = []
+    target_list = list(target)
+    for i in range(len(target_list) - n + 1):
+        output.append(tuple(target_list[i:n+i]))
+    return output
+
+
+def check_in_set(check, set_target):
+    return check in set_target
+
+
+a = "paraparaparadise"
+b = "paragraph"
+x_bi_gram = n_gram_character(a, 2)
+y_bi_gram = n_gram_character(b, 2)
+
+X = set(x_bi_gram)
+Y = set(y_bi_gram)
+print(f"和集合：{X | Y}")
+print(f"積集合：{X & Y}")
+print(f"差集合：{X - Y}")
+check = ('s', 'e')
+
+print("seがあればTrue,無いならFalse")
+print("Xは"+str(check_in_set(check, X)))
+print("Yは"+str(check_in_set(check, Y)))
